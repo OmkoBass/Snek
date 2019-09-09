6cad6a078a47a5c410cc9d395a1ab10fe089898a
OmkoBass <omkob@hotmail.com>
2019-09-09 11:58:58 +0200

    Snek

---
+++ Snake/Snake/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Snake
+{
+    public partial class Form1 : Form
+    {
+        List<Snek> snake = new List<Snek>();
+
+        void generateSnek(PaintEventArgs e)
+        {
+            Snek s = new Snek(150, 150);
+            snake.Add(s);
+            e.Graphics.FillRectangle(Brushes.Green, snake[snake.Count() - 1].X, snake[snake.Count() - 1].Y, 25, 25);
+        }
+
+        void generateFood(PaintEventArgs e)
+        {
+            Random r = new Random();
+            int x = r.Next(0, 820);
+            int y = r.Next(0, 460);
+            e.Graphics.FillEllipse(Brushes.Red, x, y, 25, 25);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            moveTimer.Start();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            generateSnek(e);
+        }
+
+        private void MoveTimer_Tick(object sender, EventArgs e)
+        {
+            for (int i = 0; i < snake.Count(); i++)
+            {
+                snake[i].X += 10;
+                snake[i].Y += 10;
+            }
+
+            Invalidate();
+        }
+    }
+}
---
+++ Snake/Snake/Snek.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Snek
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Snek(int X, int Y)
+        {
+            this.X = X;
+            this.Y = Y;
+        }
+    }
+}
